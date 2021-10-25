b5c370adba4e8778e0fa3c4ee2b61ef05689f049
DaViD0880js <75089552+DaViD0880js@users.noreply.github.com>
2021-10-25 17:49:24 -0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,10 @@
 const client = require('../index')
 module.exports = {
-    // we want a message event
+
     event: "guildMemberAdd",
-    // we want it to trigger multiple times
+
     once: false,
-    // the actual function
+  
     run(member) {
         
            let b = member.guild.channels.cache.get("875398431974256715")
